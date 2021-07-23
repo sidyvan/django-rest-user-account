12e89ac0da4beff07e99b08ac85862f64602c413
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2021-07-23 16:33:54 -0300

    Antes do deploy

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from account.models import User
+from datetime import date
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user =  User.objects.create(email="teste@teste.com", fullName="Teste", birthDate="2001-02-02", phone="(31) 986097668", document="099.556.430-21")
+
+    def test_check_count_user(self):
+
+        assert User.objects.all().count() == 1
+
