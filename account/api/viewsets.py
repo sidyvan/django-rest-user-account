fd2c2d03262e9cfcc239160ad7d63bf05535584d
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2021-07-23 10:30:23 -0300

    Metodos GET, POST e PUT implementados

--- account/api/viewsets.py
+++ account/api/viewsets.py
@@ -5,10 +5,29 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
+from account.models import User
+
+
+
 
 class UserView(APIView):
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get(self, request, format=None):
+        snippets = User.objects.all()
+        serializer = UserSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
