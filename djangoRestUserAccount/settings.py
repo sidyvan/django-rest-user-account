214c35d194b695143e67398ecff055950a99daa3
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2021-07-25 10:43:30 -0300

    upload

--- djangoRestUserAccount/settings.py
+++ djangoRestUserAccount/settings.py
@@ -85,11 +85,25 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'atarbanco',
+        'USER': 'atar',
+        'PASSWORD': 'atar',
+        'HOST': '127.0.0.1',   
+        'PORT': '3310',
     }
 }
 
